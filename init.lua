e067d6f2d7762fda54861df95c0c5cc67a5ec776
Jack Cordery <jack.redolf@gmail.com>
2026-03-08 11:30:06 +0000

    removed comment

--- init.lua
+++ init.lua
@@ -697,7 +697,6 @@ require('lazy').setup({
             },
             python = {
               analysis = {
-                -- Ignore all files for analysis to exclusively use Ruff for linting
                 -- ignore = { '*' },
               },
             },
